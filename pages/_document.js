0eda349a835cb3460d1cc796df5c461572556952
Chris Womack <womackchrisw@gmail.com>
2025-03-28 20:48:28 -0400

    switch back to .js

--- pages/_document.js
+++ pages/_document.js
@@ -6,9 +6,11 @@ class MyDocument extends Document {
 
     return (
       <Html lang="en" className="theme-compiled">
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin="true" />
-        <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Expanded:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" cross-origin="true" />
+          <link href="https://fonts.googleapis.com/css2?family=Encode+Sans+Semi+Expanded:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base overflow-hidden`}
         >
